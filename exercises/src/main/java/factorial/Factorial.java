c3b77e885715223b8da296fd4748ca5e2723d333
tania <tania.zai1010@gmail.com>
2023-05-23 22:37:41 +0300

    Factorial: javadoc

--- exercises/src/main/java/factorial/Factorial.java
+++ exercises/src/main/java/factorial/Factorial.java
@@ -1,25 +1,25 @@
 package factorial;
 
 /**
- * What is this class for? This class describes a Factorial
+ * This class calculates the Factorial funcation.
  */
 public class Factorial {
     /**
-     * What does this function do?function calculates a Factorial value for integer values
+     * Calculates the Factorial function.
      *
-     * @param n - what does it mean, which values does it accept? Variable n passed to factorial function and contains integer value
-     * @return what it returns? function return result (integer value)
-     * @exception IllegalArgumentException when is it thrown? IllegalArgumentException thrown when n < 0
+     * @param n - argument of the Factorial function
+     * @return Factorial value
+     * @throws IllegalArgumentException thrown when n < 0
      */
-    public static int factorial(int n){
+    public static int factorial(int n) {
         if (n < 0) {
-            throw new  IllegalArgumentException("n < 0");
+            throw new IllegalArgumentException("n < 0");
         }
-        if (n == 0){
+        if (n == 0) {
             return 1;
         }
         int fn = 1;
-        for (int i= 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             fn = fn * i;
 
         }
